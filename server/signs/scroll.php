c9cdb3ca47d08dd54e4bb5ebf3919ecadd0923cb
Gareth J. Greenaway <gareth@wiked.org>
2013-02-20 15:36:49 -0800

    adding new sponsorship logos.  changing scroll.php to show information for current time.  removing external hrefs in favor of local ones, eg. images.

--- server/signs/scroll.php
+++ server/signs/scroll.php
@@ -8,8 +8,8 @@ $xml = simplexml_load_file('http://www.socallinuxexpo.org/scale11x/sign.xml');
 
 $starttime = mktime(0, 0, 0, 2, 22, 2013) / 60;
 
-#$rightnow = round(time() / 60);
-$rightnow = mktime(12, 45, 0, 2, 22, 2013) / 60;
+$rightnow = round(time() / 60);
+#$rightnow = mktime(20, 30, 0, 2, 22, 2013) / 60;
 $minsafter = $rightnow - $starttime;
 
 $data = array();
@@ -50,24 +50,7 @@ foreach ($xml->node AS $node) {
 		$name = (string) $node->Speaker;
 	}
 	$data[] = array((string) $node->Day, $thistime, $name, (string) $node->Title, (string) $node->Room, (string) $node->Topic);
-	
-	/*
-	$realtime = explode(" ", $thistime);
-	$realstime = explode(" ", $thisend);
-	$handm = explode(":", $realtime[0]);
-	if ($realtime[1] == "PM" && $handm[0] != "12") {
-		$mfromm = (($handm[0] + 12) * 60) + $handm[1];
-	} else {
-		$mfromm = ($handm[0] * 60) + $handm[1];
-	}
-	$handme = explode(":", $realstime[0]);
-	if ($realstime[1] == "PM" && $handme[0] != "12") {
-		$mfromme = (($handme[0] + 12) * 60) + 60 + $handme[1];
-	} else {
-		$mfromme = ($handme[0] * 60) + 60 + $handme[1];
-	}
-	*/
-	
+		
 	$realtime = $thistime;
 	$realstime = $thisend;
 	$handm = explode(":", $realtime);
