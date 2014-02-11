54beea419cd3bf81644f1fc19dfee4499ec80803
root <root@blackoil.loc.wiked.org>
2014-02-11 15:48:25 -0800

    current state

--- server/signs/index.php
+++ server/signs/index.php
@@ -1,45 +1,45 @@
 <?php
-
-?>
-
-<?php
-  if (!empty($_GET["carousel"])) {
-    switch ($_GET["carousel"]) {
-      case 'one':
-        $carousel_array = $carousel_one;
-        break;
-      case 'two':
-        $carousel_array = $carousel_two;
-        break;
-      case 'three':
-        $carousel_array = $carousel_three;
-        break;
-      case 'four':
-        $carousel_array = $carousel_four;
-        break;                        
-      case 'five':
-        $carousel_array = $carousel_five;
-        break;
-      case 'six':
-        $carousel_array = $carousel_six;
-        break;
+  if (!empty($_GET["room"])) {
+    switch ($_GET["room"]) {
+        case 'LaJolla':
+        case 'Carmel':
+        case 'LosAngelesA':
+        case 'LosAngelesB':
+        case 'LosAngelesC':
+        case 'CenturyAB':
+        case 'CenturyCD':
+        case 'Marina':
+        case 'BelAir':
+        case 'PlazaA':
+        case 'CatalinaA':
+        case 'CatalinaC':
+        case 'CatalinaD':
+        case 'SantaMonicaB':
+        case 'SanLorenzoA':
+        case 'SanLorenzoB':
+        case 'SanLorenzoC':
+        case 'SanLorenzoD':
+        case 'SanLorenzoE':
+        case 'SanLorenzoF':
+            room($_GET["room"]);
+            break;
+        case 'CatalinaB':
+        case 'AV':
+            avnoc();
+            break;
+        default:
+            main();
+            break;
     }
-    for ($x = 0; $x < sizeof($carousel_array); $x++)
-    {
-      if ($x == 0) {
-        echo "<!-- item active -->";
-        echo "<div class='item active'>";  
-      } else {
-        echo "<!-- item -->";                
-        echo "<div class='item'>";
-      }
-      echo "<!-- item -->";              
-      printf("<div><h3>%s</h3><img class='graph-image' src='images/%s'></div>", $carousel_array[$x]['name'], $carousel_array[$x]['graph']);
-      echo "</div>";
-    }    
   } else {
+    main();
+  }
 ?>
 
+<!--- Main -->
+<?php
+function main() {
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -202,6 +202,219 @@
 </html>
 
 <?php
-  }
+}
 ?>
 
+<!--- AV NoC Room -->
+<?php
+function avnoc() {
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+    <title>SCALE 12x</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <!-- <link href="grid.css" rel="stylesheet"> -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div class="container main-container">
+
+      <div class="header row">
+        <div class="col-md-8">
+          <img src="header.png">
+        </div>
+
+        <div class="col-md-4">            
+          <ul class="clock pull-right">
+            <li><div id="h1" class="card">&nbsp;</div></li>
+            <li><div id="h2" class="card">&nbsp;</div></li>          
+            <li class="separator">:</li>
+            <li><div id="m1" class="card">&nbsp;</div></li>
+            <li><div id="m2" class="card">&nbsp;</div></li>
+            <!--
+            <li class="separator">:</li>
+            <li><div id="s1" class="card">&nbsp;</div></li>
+            <li><div id="s2" class="card">&nbsp;</div></li>
+            -->
+            <li class="separator">&nbsp;</li>          
+            <li><div id="meridiem1" class="card">&nbsp;</div></li>
+            <li><div id="meridiem2" class="card">&nbsp;</div></li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div class="row"><hr></div>
+
+      <!-- Begin Row -->
+      
+      <div class="row graph-row">
+        <div class="graph col-md-12">
+          <div id="schedule" class="row schedule"></div>
+        </div>
+      </div>
+      <!-- End Row -->
+
+      <div class="row"><hr></div>
+      <!-- End Row -->
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>    
+  <script src="bootstrap/js/bootstrap.js"></script>
+  <script src="js/clock.js"></script>
+  <script src="js/timer.js"></script>
+
+  <script type="text/javascript">
+    
+    $(document).ready(function() {
+
+      // Ensure we're not caching data
+      $.ajaxSetup ({
+        cache: false  
+      });
+      
+      updateClock();
+      setInterval('updateClock()', 1000);
+
+      // Hide the schedule until we've loaded the data
+      $('#schedule').hide();
+      
+      var loadScheduleUrl = "scroll.php";
+      $("#schedule").load(loadScheduleUrl);
+      $("#schedule").show();
+
+      /* Reload and Refresh Schedule once a minute */
+      var scheduleRefreshId = setInterval(function() {
+        //$("#schedule").fadeOut("slow").load(loadScheduleUrl).fadeIn("slow");
+        $("#schedule").load(loadScheduleUrl);        
+      }, 60000);
+      
+    });
+        
+    </script>
+  </body>
+</html>
+
+<?php
+}
+?>
+
+<!--- Individual Room Display -->
+<?php
+function room($room) {
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+    <title>SCALE 12x: <?php echo "$room"; ?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <!-- <link href="grid.css" rel="stylesheet"> -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div class="container main-container">
+      <div class="header row" style="text-align: center;">
+        <div class="col-md-12">
+          <img src="header.png">
+        </div>
+      </div>
+      <!-- <div class="row roomHeader"><h3>Coming Up</h3></div> -->
+      <div class="row roomHeader"><h3><?php echo $room; ?></h3></div>
+
+      <!-- Begin Row -->
+      <div class="row graph-row">
+        <div class="graph col-md-12">
+          <div id="schedule" class="row schedule"></div>
+        </div>
+      </div>
+      <!-- End Row -->
+
+    </div> <!-- /container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>    
+  <script src="bootstrap/js/bootstrap.js"></script>
+  <script src="js/clock.js"></script>
+  <script src="js/timer.js"></script>
+
+  <script type="text/javascript">
+    
+    $(document).ready(function() {
+
+      // Ensure we're not caching data
+      $.ajaxSetup ({
+        cache: false  
+      });
+      
+      // Hide the schedule until we've loaded the data
+      $('#schedule').hide();
+      
+      var loadScheduleUrl = "room.php?room=<?php echo $room; ?>";
+      $("#schedule").load(loadScheduleUrl);
+      $("#schedule").show();
+
+      /* Reload and Refresh Schedule once a minute */
+      var scheduleRefreshId = setInterval(function() {
+        //$("#schedule").fadeOut("slow").load(loadScheduleUrl).fadeIn("slow");
+        $("#schedule").load(loadScheduleUrl);        
+      }, 60000);
+      
+    });
+        
+    </script>
+  </body>
+</html>
+
+<?php
+}
+?>
