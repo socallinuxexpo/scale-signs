72daddbcaadaf6af2af7e8119f418f87ae5891fe
Gareth J. Greenaway <gareth@wiked.org>
2015-02-07 12:44:43 -0800

    More code to force the date.

--- archive/server/signs/room_map.php
+++ archive/server/signs/room_map.php
@@ -2,7 +2,6 @@
 $room_map = array(
         "172.16.13.1" => array('type' => "noc", 'orientation' => 'vertical'),
         "10.10.30.59" => array('type' => "noc", 'orientation' => 'vertical'),
-        "10.8.0.2" => array('type' => "noc", 'orientation' => 'vertical'),
         "127.0.0.1" => array('type' => "room", 'room' => 'LaJolla'),
         );
 ?>
