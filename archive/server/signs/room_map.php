1c1b04da4b2926bb72da230bead4b34dfbc1e6f2
Gareth J. Greenaway <gareth@wiked.org>
2015-02-09 20:52:00 -0800

    Updating code to show sessions that are longer than one hour.  Adding colors for for missing topics.

--- archive/server/signs/room_map.php
+++ archive/server/signs/room_map.php
@@ -3,6 +3,6 @@ $room_map = array(
         "172.16.13.21" => array('type' => "noc", 'orientation' => 'vertical'),
         "10.10.30.59" => array('type' => "noc", 'orientation' => 'vertical'),
         #"127.0.0.1" => array('type' => "room", 'room' => 'LaJolla'),
-        "10.8.0.2" => array('type' => "noc", 'room' => 'LaJolla'),
+        #"10.8.0.2" => array('type' => "noc", 'room' => 'LaJolla'),
         );
 ?>
