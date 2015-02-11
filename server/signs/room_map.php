0a3c6bdb6a9bd3e0bdc5a7e249301397e7312673
Gareth J. Greenaway <gareth@wiked.org>
2015-02-10 22:14:19 -0800

    Adding IPs for signs.

--- server/signs/room_map.php
+++ server/signs/room_map.php
@@ -1,8 +1,14 @@
 <?php
 $room_map = array(
-        "172.16.13.21" => array('type' => "noc", 'orientation' => 'vertical'),
-        "10.10.30.59" => array('type' => "noc", 'orientation' => 'vertical'),
-        #"127.0.0.1" => array('type' => "room", 'room' => 'LaJolla'),
-        #"10.8.0.2" => array('type' => "noc", 'room' => 'LaJolla'),
+        "172.16.13.21" => array('type' => "schedule", 'orientation' => 'horizontal'),
+        "172.16.13.22" => array('type' => "schedule", 'orientation' => 'horizontal'),
+        "172.16.13.23" => array('type' => "schedule", 'orientation' => 'horizontal'),
+        "172.16.13.24" => array('type' => "schedule", 'orientation' => 'horizontal'),
+        "172.16.13.25" => array('type' => "noc", 'orientation' => 'horizontal'),
+        "172.16.13.26" => array('type' => "schedule", 'orientation' => 'horizontal'),
+        "172.16.13.27" => array('type' => "schedule", 'orientation' => 'horizontal'),
+        "172.16.13.28" => array('type' => "schedule", 'orientation' => 'horizontal'),
+        "172.16.13.29" => array('type' => "schedule", 'orientation' => 'horizontal'),
+        "172.16.13.30" => array('type' => "schedule", 'orientation' => 'horizontal'),
         );
 ?>
