1a90df0964dc299f0c5b258c9e386d98f9c50c4b
Gareth J. Greenaway <gareth.greenaway@reachlocal.com>
2015-02-18 22:31:22 -0800

    switching marina back to schedule.

--- server/signs/room_map.php
+++ server/signs/room_map.php
@@ -5,7 +5,7 @@ $room_map = array(
         "172.16.13.23" => array('type' => "schedule", 'orientation' => 'horizontal'), # registration
         "172.16.13.24" => array('type' => "schedule", 'orientation' => 'horizontal'), # san-lorenzo
         "172.16.13.25" => array('type' => "noc", 'orientation' => 'vertical'), # noc
-        "172.16.13.26" => array('type' => "noc", 'orientation' => 'horizontal'), #marina
+        "172.16.13.26" => array('type' => "schedule", 'orientation' => 'horizontal'), #marina
         "172.16.13.27" => array('type' => "schedule", 'orientation' => 'horizontal'),
         "172.16.13.28" => array('type' => "schedule", 'orientation' => 'horizontal'),
         "172.16.13.29" => array('type' => "schedule", 'orientation' => 'horizontal'),
