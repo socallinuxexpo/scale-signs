b7fb5eddd54f87df4e94078867d1190e2e817d27
Gareth J. Greenaway <gareth.greenaway@reachlocal.com>
2015-02-18 20:34:19 -0800

    Switching NOC sign from horizontal to vertical.

--- archive/server/signs/room_map.php
+++ archive/server/signs/room_map.php
@@ -4,7 +4,7 @@ $room_map = array(
         "172.16.13.22" => array('type' => "schedule", 'orientation' => 'horizontal'),
         "172.16.13.23" => array('type' => "schedule", 'orientation' => 'horizontal'),
         "172.16.13.24" => array('type' => "schedule", 'orientation' => 'horizontal'),
-        "172.16.13.25" => array('type' => "noc", 'orientation' => 'horizontal'),
+        "172.16.13.25" => array('type' => "noc", 'orientation' => 'vertical'),
         "172.16.13.26" => array('type' => "schedule", 'orientation' => 'horizontal'),
         "172.16.13.27" => array('type' => "schedule", 'orientation' => 'horizontal'),
         "172.16.13.28" => array('type' => "schedule", 'orientation' => 'horizontal'),
