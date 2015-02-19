fcbfaa32223ccf1ae3cbca8dcff9d2b0341b88d1
Gareth J. Greenaway <gareth.greenaway@reachlocal.com>
2015-02-18 21:14:56 -0800

    update type of marina sign.

--- archive/server/signs/room_map.php
+++ archive/server/signs/room_map.php
@@ -1,11 +1,11 @@
 <?php
 $room_map = array(
-        "172.16.13.21" => array('type' => "schedule", 'orientation' => 'horizontal'),
-        "172.16.13.22" => array('type' => "schedule", 'orientation' => 'horizontal'),
-        "172.16.13.23" => array('type' => "schedule", 'orientation' => 'horizontal'),
-        "172.16.13.24" => array('type' => "schedule", 'orientation' => 'horizontal'),
-        "172.16.13.25" => array('type' => "noc", 'orientation' => 'vertical'),
-        "172.16.13.26" => array('type' => "schedule", 'orientation' => 'horizontal'),
+        "172.16.13.21" => array('type' => "schedule", 'orientation' => 'horizontal'), # basement
+        "172.16.13.22" => array('type' => "schedule", 'orientation' => 'horizontal'), # show-lobby
+        "172.16.13.23" => array('type' => "schedule", 'orientation' => 'horizontal'), # registration
+        "172.16.13.24" => array('type' => "schedule", 'orientation' => 'horizontal'), # san-lorenzo
+        "172.16.13.25" => array('type' => "noc", 'orientation' => 'vertical'), # noc
+        "172.16.13.26" => array('type' => "noc", 'orientation' => 'horizontal'), #marina
         "172.16.13.27" => array('type' => "schedule", 'orientation' => 'horizontal'),
         "172.16.13.28" => array('type' => "schedule", 'orientation' => 'horizontal'),
         "172.16.13.29" => array('type' => "schedule", 'orientation' => 'horizontal'),
