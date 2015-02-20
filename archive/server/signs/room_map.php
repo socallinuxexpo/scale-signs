7984aaec5f6f101ccce75006f7ab0283ff922947
Gareth J. Greenaway <gareth.greenaway@reachlocal.com>
2015-02-20 12:03:15 -0800

    noc sign hoziontal.

--- archive/server/signs/room_map.php
+++ archive/server/signs/room_map.php
@@ -6,7 +6,7 @@ $room_map = array(
         "172.16.13.24" => array('type' => "schedule", 'orientation' => 'horizontal'), # san-lorenzo
         "172.16.13.25" => array('type' => "noc", 'orientation' => 'vertical'), # noc
         "172.16.13.26" => array('type' => "schedule", 'orientation' => 'horizontal'), #marina
-        "172.16.13.27" => array('type' => "schedule", 'orientation' => 'horizontal'),
+        "172.16.13.27" => array('type' => "noc", 'orientation' => 'horizontal'),
         "172.16.13.28" => array('type' => "schedule", 'orientation' => 'horizontal'),
         "172.16.13.29" => array('type' => "schedule", 'orientation' => 'horizontal'),
         "172.16.13.30" => array('type' => "schedule", 'orientation' => 'horizontal'),
