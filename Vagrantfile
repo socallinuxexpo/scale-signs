7bae94ad93a84b30423f10285ea43defbc026ac2
kylerisse <kylerisse@gmail.com>
2017-02-26 16:39:10 -0800

    15x image changes

---
+++ Vagrantfile
@@ -0,0 +1,21 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+
+  config.vm.box = "geerlingguy/centos7"
+
+  config.vm.network "private_network", ip: "192.168.168.168"
+
+  config.vm.synced_folder "./server/signs", "/var/www/html/signs"
+  config.vm.synced_folder "./noc", "/var/www/html/noc"
+
+  config.vm.provision "shell", inline: <<-SHELL
+    yum update -y
+    yum install httpd php -y
+    ifup enp0s8
+    service httpd start
+    echo "visit http://192.168.168.168/signs"
+  SHELL
+
+end
--- server/signs/sponsors.php
+++ server/signs/sponsors.php
@@ -2,33 +2,97 @@
   if (!empty($_GET["group"])) {
     switch ($_GET["group"]) {
       case 'one':
-        $logos = range(1, 66);
+        $logos = array(
+                      "anchore",
+                      "aredn",
+                      "bitnami",
+                      "blackduck",
+                      "chef",
+                      "collabnet",
+                      "coreos",
+                      "couchbase",
+                      "cyberark",
+                      "datadog",
+                      "dellemc",
+                      "disney",
+                      "docker",
+                      "everbridge",
+                      "facebook",
+                      "flexera",
+                      "hpe",
+                      "jfrog",
+                      "linode",
+                      "linuxacademy",
+                      "linuxfoundation",
+                      "linuxpro",
+                      "mediatemple",
+                      "minio",
+                      "mysql",
+                      "nats",
+                      "netapp",
+                      "nginx",
+                      "openshift",
+                      "opensource",
+                      "openx",
+                      "opsgenie",
+                      "orabuntu",
+                      "oreilly",
+                      "percona",
+                      "platform9",
+                      "postgresql",
+                      "procore",
+                      "pssclabs",
+                      "q",
+                      "r1soft",
+                      "rancher",
+                      "redhat",
+                      "resinio",
+                      "saucelabs",
+                      "signalsciences",
+                      "sparkpost",
+                      "stackiq",
+                      "steelhouse",
+                      "suse",
+                      "symantec",
+                      "threatstack",
+                      "ticketmaster",
+                      "ubuntu",
+                      "usenix",
+                      "verizon",
+                      "versionone",
+                      "victorops",
+                      "vikidial",
+                      "wavefront",
+                      "yahoo",
+                      "yocto",
+                      "zabbix",
+                    );
         break;
-      case 'two':
-        $logos = range(67, 133);
-        break;
+      //case 'two':
+      //  $logos = range(67, 133);
+      //  break;
     }
   }
 ?>
 
 <div id="carousel1" class="sponsorCarousel carousel carousel-fade">
 <div class="carousel-inner">
-<?php 
-  shuffle($logos);  
+<?php
+  shuffle($logos);
   for ($i = 0; $i < (count($logos) - 1); $i++) {
     $logo = array_pop($logos);
 ?>
     <div class="item sponsor-logo <?php if ( $i == 0 ) { echo 'active'; } ?>" >
-        <img class="" src="images/sponsors/15x/<?php if ( $logo < 10 ) { echo "0" . $logo; } else { echo "$logo"; } ?>.png">
+        <img class="" src="images/sponsors/15x/<?php echo "$logo"; ?>.png">
     </div>
 <?php
    }
-?>      
+?>
 </div>
 </div>
 
 <script src="js/jquery-1.10.2.min.js"></script>
-<script src="js/jquery.marquee.js"></script>    
+<script src="js/jquery.marquee.js"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
 <script type="text/javascript">
 
@@ -37,5 +101,5 @@
         interval: 10000
       });
   });
-      
+
 </script>
