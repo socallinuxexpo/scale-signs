da6ba5a798e9b0b79728112a8cdea9fbeddd9876
kylerisse <kylerisse@gmail.com>
2017-02-28 15:23:52 -0800

    updated urls for 15x

--- archive/client/pi-python/usr/bin/web.py
+++ archive/client/pi-python/usr/bin/web.py
@@ -42,7 +42,7 @@ webview.get_settings().props.javascript_can_open_windows_automatically = True
 
 webview.connect('close-web-view', close_func)
 webview.connect('create-web-view', create_func)
-webview.open('http://signs.expo.socallinuxexpo.org/index.php')
+webview.open('http://signs.scale.lan/signs')
 #webview.open('http://signs.socallinuxexpo.org/index.php?year=2016&month=1&day=21&hour=10&minute=30')
 
 
