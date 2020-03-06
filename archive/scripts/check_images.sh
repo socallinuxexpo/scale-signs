b1b8b03afe62e5aa2ef4b8cb3d9268e77c5ce1f9
kylerisse <kylerisse@users.noreply.github.com>
2020-03-06 12:58:05 -0800

    add circle ci for image checks

--- archive/scripts/check_images.sh
+++ archive/scripts/check_images.sh
@@ -2,18 +2,24 @@
 
 bad=0
 
-for i in `cat server/sponsors.php | grep png | cut -d '"' -f 2`; do
+c=0
+for i in `cat server/sponsors.php | grep 'png\|jpg' | cut -d '"' -f 2`; do
 	if [ ! -f server/images/sponsors/$i ]; then
 		echo "from sponsors.php: $i is missing";
 		bad=1
 	fi;
+	c=`expr $c + 1`
 done;
+echo "checked $c files in sponsors.php"
 
-for i in `cat server/room.php | grep png | grep '=>' | cut -d '"' -f 4`; do
+c=0
+for i in `cat server/room.php | grep 'png\|jpg' | grep '=>' | cut -d '"' -f 4`; do
 	if [ ! -f server/images/sponsors/$i ]; then
 		echo "from room.php: $i is missing";
 		bad=1
 	fi;
+	c=`expr $c + 1`
 done;
+echo "checked $c files in room.php"
 
 exit $bad
