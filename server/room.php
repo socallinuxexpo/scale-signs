af4fe2212bae8ec33ba5230b0bc109fb2268cc8d
Robert James Hernandez <rob@sarcasticadmin.com>
2022-07-26 12:21:50 -0700

    conference prep: s/18x/19x/g

--- server/room.php
+++ server/room.php
@@ -340,7 +340,7 @@ $sponsors_to_rooms = array(
     ),
 );
 
-$url = 'http://www.socallinuxexpo.org/scale/18x/sign.xml';
+$url = 'http://www.socallinuxexpo.org/scale/19x/sign.xml';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
