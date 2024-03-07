b358e471d47fca2f11d474c02a355a994b2f96c1
Robert James Hernandez <rob@sarcasticadmin.com>
2024-03-07 17:27:48 +0000

    constants test

---
+++ server/constants.php
@@ -0,0 +1,8 @@
+date_default_timezone_set('America/Los_Angeles');
+
+# set yearly (change if DST starts during SCaLE)
+# before "spring forward"
+
+# after "spring forward"
+# 21x
+$starttime = mktime(0, 0, 0, 3, 14, 2024) / 60;
